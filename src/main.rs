e9e9865b93a89d7e0a9b9ff04b214a12023c2f9b
Justin Marquez <justin.marquez@wisc.edu>
2026-01-30 17:56:09 -0600

    refactor: call get_appname for cache directory

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use clap::Parser;
 use rustemon::client::RustemonClient;
-use utils::cli::{Args, SubArgs};
+use utils::cli::{Args, SubArgs, get_appname};
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +12,7 @@ async fn main() {
   // Create cache directory for API calls
   if let None = args.cache_dir {
     args.cache_dir = match std::env::home_dir() {
-      Some(path) => Some(format!("{}/.cache/pokefilter", path.display()).into()),
+      Some(path) => Some(format!("{}/.cache/{}", path.display(), get_appname()).into()),
       None => None,
     }
   }
