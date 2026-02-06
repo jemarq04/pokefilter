4c07a29b2dba09e49930421dd15840a8a7eaaf30
Justin Marquez <justin.marquez@wisc.edu>
2026-02-05 20:25:42 -0600

    feat: add initial structure for CSV records

--- src/filter/list.rs
+++ src/filter/list.rs
@@ -2,6 +2,20 @@ use crate::utils::{cli, helpers};
 use clap::error::ErrorKind;
 use std::{fs::File, path::Path};
 
+struct Record {
+  identifier: String,
+  dex: i64,
+  pokemon: String,
+  species: String,
+  generation: i64,
+  types: Vec<String>,
+  abilities: Vec<String>,
+  color: String,
+  egg_groups: Vec<String>,
+  held_items: Vec<String>,
+  growth_rate: String,
+}
+
 pub fn list(filepath: Option<std::path::PathBuf>) -> Result<(), clap::Error> {
   // If absent, set filepath to default
   let filepath = match filepath {
