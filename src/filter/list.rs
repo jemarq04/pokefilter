94b84d970d56a94c42081ec7859f2c95fb011aae
Justin Marquez <justin.marquez@wisc.edu>
2026-02-10 09:00:22 -0600

    refactor: Revert "lint: add guards for certain pedantic lints"
    This reverts commit f883c096d5085911257d25ac0ba8b75339b3dca6.

--- src/filter/list.rs
+++ src/filter/list.rs
@@ -3,7 +3,6 @@ use clap::error::ErrorKind;
 use serde::{self, Deserialize, de};
 use std::{fs::File, path::Path};
 
-#[allow(clippy::struct_excessive_bools)]
 #[derive(Clone, Debug, Deserialize)]
 struct Record {
   identifier: String,
